5c15d3340d6f8a2c64a21260f40c6eb57952488f
Marianalevs <106361499+Marianalevs@users.noreply.github.com>
2022-08-01 18:08:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace PruebaVector10
+{
+    class PruebaVector10
+    {
+        private string[] nombres;
+        private int[] edades;
+
+        public void Cargar()
+        {
+            nombres = new string[5];
+            edades = new int[5];
+            for (int r = 0; r < nombres.Length; r++)
+            {
+                Console.Write("ingrese nombre:");
+                nombres[r] = Console.ReadLine();
+                Console.Write("ingrese edad:");
+                string linea;
+                linea = Console.ReadLine();
+                edades[r] = int.Parse(linea);
+            }
+        }
+        public void MayoresEdad()
+        {
+            Console.WriteLine("Personas mayores de edad.");
+            for (int r = 0; r < nombres.Length; r++)
+            {
+                if (edades[r] > 18)
+                {
+                    Console.WriteLine(nombres[r]);
+                }
+                Console.ReadKey();
+            }
+            static void Main(string[] args)
+            {
+                PruebaVector10 pv = new PruebaVector10();
+                pv.Cargar();
+                pv.MayoresEdad();
+            }
+        }
+    }
+}
+    
+
+        
+    
+
